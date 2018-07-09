1998ff69cc1a2bec7518fc54e9b2e33cd1a2e618
唐群 <tangqun@9hcom.com>
2018-07-09 23:53:00 +0800

    add  miniprogram-admin module

---
+++ miniprogram-admin/src/main/java/com/kingworld/miniprogram/admin/MiniprogramAdminApplication.java
@@ -0,0 +1,12 @@
+package com.kingworld.miniprogram.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprogramAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprogramAdminApplication.class, args);
+	}
+}
